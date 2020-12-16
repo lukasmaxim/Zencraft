70509809fbf4039dc2de3316b00407efc35aa16e
Lukas Maximilian Masopust <l.masopust@me.com>
2020-12-16 18:10:25 +0100

    Added properties, offset lookup

--- src/models/convert.py
+++ src/models/convert.py
@@ -2,6 +2,7 @@ import nbtlib
 import json
 import os
 import glob
+from offset_lookup import offset_lookup
 
 cwd = os.getcwd()
 
@@ -14,24 +15,43 @@ for filename in os.listdir(in_dir):
 
     to_json = {
         "dimensions": [],
+        "offset": None,
         "structure": []
     }
 
     with nbtlib.load(in_dir + filename) as nbtfile:
         root = nbtfile.root
-        
+
         size = root.get('size')
         blocks = root.get('blocks')
         palette = root.get('palette')
-        
+
         for i, material in enumerate(palette):
             to_json["dimensions"] = [size[0], size[1], size[2]]
-            to_json["structure"].append({"material":str(material["Name"]), "position": []})
-            
+            to_json["offset"] = offset_lookup.get(filename.split('.')[0], "not in lookup")
+            material_string = str(material["Name"])
+
+            if "Properties" in material:
+                properties = material["Properties"]
+
+                material_string += "["
+
+                for prop in properties:
+                    value = material["Properties"].get(prop)
+
+                    material_string = material_string + prop + "=" + value + ","
+
+                material_string = material_string[:-1]
+                material_string += "]"
+
+            to_json["structure"].append(
+                {"material": material_string, "position": []})
+
             for block in blocks:
                 if block["state"] == i:
                     pos = block["pos"]
-                    to_json["structure"][i]["position"].append([int(pos[0]), int(pos[1]), int(pos[2])])
+                    to_json["structure"][i]["position"].append(
+                        [int(pos[0]), int(pos[1]), int(pos[2])])
 
         outname = filename.split('.')[0] + '.json'
 
